566a0b99800704e5a5df1b338255b38ad58d555a
ben-wiersma <benkwiersma@gmail.com>
2023-03-21 22:12:45 -0700

    Finish transcriptome hash table

--- project.py
+++ project.py
@@ -1,11 +1,50 @@
 from Bio import SeqIO
-for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
-    print(seq_transcriptome.id)
-    print(repr(seq_transcriptome.seq))
-    print(len(seq_transcriptome))
 
+
+
+
+'''
+for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
+    #print(seq_transcriptome.id)
+    print(segment(seq_transcriptome.seq, 10))
+    #print(len(seq_transcriptome))
+'''
+
+
+
+    
+
+
+print("running")
 
 
 def pseudoalignment(transcriptome, k):
-    print("hello world")
 
+    transcriptome_dict = dict()
+
+    for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
+        for i in range(0, len(seq_transcriptome) - (k + 1)):
+
+            seq_str = str(seq_transcriptome.seq)[i:(k + i)]
+
+            if seq_str not in transcriptome_dict:
+                transcriptome_dict[seq_str] = {seq_transcriptome.id}
+            else:
+                transcriptome_dict[seq_str].add(seq_transcriptome.id)
+
+
+    print(list(transcriptome_dict.items())[:10])
+    
+pseudoalignment("bruh", 30)
+
+'''
+
+output = list()
+string = "hellothere"
+
+for i in range(0, len(string) - 2):
+    output.append(string[i:(i+3)])
+
+print(output)
+
+'''
