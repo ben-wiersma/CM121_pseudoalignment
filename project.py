24c5afcec5883e10ca508ef512caad2e8bba6bc5
ben-wiersma <benkwiersma@gmail.com>
2023-03-23 02:44:14 -0700

    Add equivalence class mapping

--- project.py
+++ project.py
@@ -1,50 +1,78 @@
 from Bio import SeqIO
-
-
-
-
-'''
-for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
-    #print(seq_transcriptome.id)
-    print(segment(seq_transcriptome.seq, 10))
-    #print(len(seq_transcriptome))
-'''
-
-
-
-    
-
+import pandas as pd
 
 print("running")
 
 
-def pseudoalignment(transcriptome, k):
+def pseudoalignment(k):
+
+    #define global variables
 
     transcriptome_dict = dict()
 
-    for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
-        for i in range(0, len(seq_transcriptome) - (k + 1)):
+    equivalence_dict = dict()
 
-            seq_str = str(seq_transcriptome.seq)[i:(k + i)]
+    data = pd.DataFrame(columns=["counts","number of items", "isoforms"])
+
+
+    #create transcriptome hash table
+
+    for seq_transcriptome in SeqIO.parse("chr11_transcriptome.fasta", "fasta"):
+
+        for i in range(0, len(seq_transcriptome) - k + 1):
+
+            seq_str = str(seq_transcriptome.seq)[i:(k + i)] # create k-mer
+
+            seq_comp = str(seq_transcriptome.seq.reverse_complement())[i:(k + i)] # create reverse complement k-mer
 
             if seq_str not in transcriptome_dict:
                 transcriptome_dict[seq_str] = {seq_transcriptome.id}
             else:
                 transcriptome_dict[seq_str].add(seq_transcriptome.id)
 
+            if seq_comp not in transcriptome_dict:
+                transcriptome_dict[seq_comp] = {seq_transcriptome.id}
+            else:
+                transcriptome_dict[seq_comp].add(seq_transcriptome.id)
 
-    print(list(transcriptome_dict.items())[:10])
     
-pseudoalignment("bruh", 30)
+    # map equivalence classes
+    
+    for seq_read in SeqIO.parse("reads.fasta", "fasta"):
 
-'''
+        equivalence = set()
 
-output = list()
-string = "hellothere"
+        for i in range(0, len(seq_read) - k + 1):
 
-for i in range(0, len(string) - 2):
-    output.append(string[i:(i+3)])
+            seq_str = str(seq_read.seq)[i:(k + i)]
 
-print(output)
+            if seq_str in transcriptome_dict:
+                
+                if(len(equivalence) == 0 ):
+                    equivalence = transcriptome_dict[seq_str]
+                else:
+                    equivalence = equivalence.intersection(transcriptome_dict[seq_str])
+                
+        equivalence_frozen = frozenset(equivalence)
+        
+        if equivalence_frozen not in equivalence_dict:
+            equivalence_dict[equivalence_frozen] = 1
+        else:
+            equivalence_dict[equivalence_frozen] += 1
 
-'''
+
+    # add data to dataframe
+    
+    for isoform, count in equivalence_dict.items():
+        data.loc[len(data)] = [count, len(isoform), ','.join(isoform)]
+
+    # save data to tsv
+
+    data.sort_values(by=['number of items']).to_csv('data.tsv', sep='\t', header=False, index=False)
+
+
+
+
+
+    
+pseudoalignment(30)
